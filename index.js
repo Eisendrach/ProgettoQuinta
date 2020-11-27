03f44f972940edf149cc760dc698fc2bc904d740
Eisendrach <manuel.barbieri16@gmail.com>
2020-11-27 13:37:55 +0100

    modificato il codice per seguire la consegna basata su valori degli headers e della query

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8000;
+
+app.get("/", (req, res)=>{
+  const headers = req.headers
+  res.send("WELCOME FELLOW " + headers["user-agent"] + " USER, THIS PAGE IS EMPTY FOR NOW, TRY LOOKING AT THE /greetings ENDPOINT!")
+})
+
+app.get("/greetings", (req, res) => {
+  const query = req.query
+  const lang = req.get("accept-language").substr(0, 2)
+  if(lang==="it"){
+    const message = "Ciao " + query.name + " " + query.surname  + "ed hai " + query.age + "anni"
+    res.send(message)
+  }else{
+    const message = "Hello" + query.name + " " + query.surname  + " and you are " + query.age + "years old"
+    res.send(message)
+  }
+})
+
+app.listen(8000, console.log("working as intended"))
+
+/*
 const handlebars = require('express-handlebars');
 app.set('view engine', 'hbs');
 app.engine('hbs', handlebars({
@@ -31,10 +52,12 @@ fakeApi = () => {
         lane: 'midlaner'
       }
     ];
-  }
+}
 
 app.get('/', (req, res) => {
     res.render('main', {layout: 'index', suggestedChamps: fakeApi(), listExists: true});
-    });
+});
 
 app.listen(port, () => console.log(`App listening to port ${port}`));
+*/
+
